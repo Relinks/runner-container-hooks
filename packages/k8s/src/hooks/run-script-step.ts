155474441d348ccb54490a83ec2f94974a68a05b
Jeroen Jans <jeroen.jans@sawiday.com>
2025-01-28 12:53:35 +0100

    Add logic to copy the data without using a shared volume

--- packages/k8s/src/hooks/run-script-step.ts
+++ packages/k8s/src/hooks/run-script-step.ts
@@ -2,8 +2,8 @@
 import * as fs from 'fs'
 import * as core from '@actions/core'
 import { RunScriptStepArgs } from 'hooklib'
-import { execPodStep } from '../k8s'
-import { writeEntryPointScript } from '../k8s/utils'
+import { cpFromPod, cpToPod, execPodStep } from '../k8s'
+import { usePodCpVolume, writeEntryPointScript } from '../k8s/utils'
 import { JOB_CONTAINER_NAME } from './constants'
 
 export async function runScriptStep(
@@ -20,6 +20,11 @@ export async function runScriptStep(
     environmentVariables
   )
 
+  if (usePodCpVolume()) {
+    core.debug('Copying the script to the work volume on the workflow pod')
+    await cpToPod(state.jobPod, JOB_CONTAINER_NAME, '/home/runner/_work/_temp/.', '/__w/_temp')
+  }
+
   args.entryPoint = 'sh'
   args.entryPointArgs = ['-e', containerPath]
   try {
@@ -34,5 +39,8 @@ export async function runScriptStep(
     throw new Error(`failed to run script step: ${message}`)
   } finally {
     fs.rmSync(runnerPath)
+
+    core.debug('Copying output files back to the runner')
+    await cpFromPod(state.jobPod, JOB_CONTAINER_NAME, '/__w/_temp/_runner_file_commands/.', '/home/runner/_work/_temp/_runner_file_commands')
   }
 }
